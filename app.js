ac2d55f9bb92db32ce9941f98cdbb82bdc011e63
Heaven Aldrico <4d3.h34v3n@gmail.com>
2024-12-07 03:46:45 +0700

    add cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const init = async () => {
     const path = require("path");
     const cookieParser = require("cookie-parser");
     const logger = require("morgan");
+    const cors = require("cors");
 
     const model = require("./model");
     const indexRouter = require("./routes/index");
@@ -18,6 +19,7 @@ const init = async () => {
         process.exit(1);
     }
 
+    app.use(cors());
     app.use(logger("dev"));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
